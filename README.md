9507b6ae6853c1e11b3bd16b5aff8bcca0ebb125
MaYushan <levimacn@qq.com>
2023-12-25 23:18:48 +0800

    新增 阻止非用户触发的登录框弹出

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# block-bilibili-automatic-login-pop-up
-屏蔽哔哩哔哩播放视频时自动弹出登录
+# 哔哩哔哩 阻止侵入式的登录提示
+此脚本只做两件事:  
++ 阻止非用户触发的登录框弹出  
++ 阻止非用户触发的视频暂停  
 
 [查看脚本](https://github.com/mayushans/block-bilibili-automatic-login-pop-up/blob/main/user.js)
 
-[屏蔽哔哩哔哩播放视频时自动弹出登录 - Greasy Fork](https://greasyfork.org/zh-CN/scripts/482855-%E5%B1%8F%E8%94%BD%E5%93%94%E5%93%A9%E5%93%94%E5%93%A9%E6%92%AD%E6%94%BE%E8%A7%86%E9%A2%91%E6%97%B6%E8%87%AA%E5%8A%A8%E5%BC%B9%E5%87%BA%E7%99%BB%E5%BD%95)
+[哔哩哔哩安静游客 - Greasy Fork](https://greasyfork.org/zh-CN/scripts/482855)
 
-使用 TamperMonkey、GreasyMonkey 导入脚本即可使用本脚本。
+使用浏览器插件 GreasyMonkey、Violentmonkey、TamperMonkey 导入脚本即可使用本脚本。
 
--- user.js
+++ user.js
@@ -1,21 +1,47 @@
 // ==UserScript==
-// @name         屏蔽哔哩哔哩播放视频时自动弹出登录
+// @name         哔哩哔哩 阻止侵入式的登录提示
 // @namespace    http://www.mayushan.com/
 // @version      0.1.0
-// @description  去掉播放视频时烦人的弹出登录
+// @description  阻止B站侵入式的登录提醒
 // @author       MaYushan
 // @match        https://www.bilibili.com/video/*
 // @icon         https://www.bilibili.com/favicon.ico?v=1
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    var oldPause = window.player.pause
-    window.player.pause = function(){
-        if(new Error().stack.includes("loginVersionEveryPlayInternval")){
-            throw "\n陈睿: 你所热爱的，就是你的生活。\n蒙古上单: 你 ( )什么时候( )啊？";
-        }
-        return oldPause()
-    }
+(function () {
+  "use strict";
+  // 阻止非用户触发的登录框弹出
+  var MiniLoginObject = null;
+  Object.defineProperty(window, "MiniLogin", {
+    set: (val) => {
+      MiniLoginObject = val;
+    },
+    get: () => {
+      return new Proxy(function () {}, {
+        construct() {
+          if (new Error().stack.includes("addEventListener")) {
+            return new MiniLoginObject();
+          }
+          throw "\n陈睿: 你所热爱的，就是你的生活。\n蒙古上单: 你 ( )什么时候( )啊？";
+        },
+      });
+    },
+  });
+
+  // 阻止非用户触发的视频暂停
+  if (window.location.href.includes("/video/") == true) {
+    var oldPause = window.player.pause;
+    window.player.pause = function () {
+      if (new Error().stack.includes("loginVersionEveryPlayInternval")) {
+        throw "\n陈睿: 你所热爱的，就是你的生活。\n蒙古上单: 你 ( )什么时候( )啊？";
+      }
+      return oldPause();
+    };
+  }
+
+  var styleNode = document.createElement("style");
+  styleNode.textContent = `.adblock-tips,.bili-header__bar .v-popover,.vip-login-tip,.login-tip{ display: none !important; }`;
+  var targ = document.getElementsByTagName("head")[0] || document.body || document.documentElement;
+  targ.appendChild(styleNode);
 })();
