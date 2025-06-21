05ee458a168767e035f5d4264a4bb16f5ff9201a
MaYushans <levimacn@qq.com>
2025-06-21 17:40:18 +0800

    更新拦截逻辑

--- user.js
+++ user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         哔哩哔哩 阻止侵入式的登录提示
 // @namespace    http://www.mayushan.com/
-// @version      0.1.3
-// @description  阻止B站侵入式的登录提醒
+// @version      0.1.5
+// @description  阻止B站非用户触发的登录提醒
 // @author       MaYushan
 // @match        https://www.bilibili.com/*
 // @icon         https://www.bilibili.com/favicon.ico?v=1
@@ -10,34 +10,50 @@
 // @grant        none
 // ==/UserScript==
 
+
 (function () {
   "use strict";
   // 阻止非用户触发的登录框弹出
   var MiniLoginObject = null;
+  var fakeMiniLogin = function(){}
+  fakeMiniLogin.prototype.showComponent = function(){}
+  fakeMiniLogin.prototype.addEventListener = function(){}
   Object.defineProperty(window, "MiniLogin", {
     set: (val) => {
       MiniLoginObject = val;
     },
     get: () => {
-      return new Proxy(function () {}, {
-        construct() {
-          if (new Error().stack.includes("addEventListener")) {
-            return new MiniLoginObject();
-          }
-          throw "\n陈睿: 你所热爱的，就是你的生活。\n蒙古上单: 你 ( )什么时候( )啊？";
-        },
-      });
+      if (
+        new Error().stack.includes("loginVersionEveryPlayInternval")
+        || new Error().stack.includes("loginVersionBackBlock")
+        || new Error().stack.includes("loginVersionMultiEveryPlay")
+        || new Error().stack.includes("loginVersionForce")
+        || new Error().stack.includes("loginVersionWechat")
+      ) {
+        console.log("\n陈睿: 你所热爱的，就是你的生活。\n蒙古上单: 你 ( )什么时候( )啊？");
+        return fakeMiniLogin;
+      }else{
+        return MiniLoginObject;
+      }
     },
   });
 
-  // 阻止非用户触发的视频暂停
+    // 阻止非用户触发的视频暂停
   if (window.location.href.includes("/video/") == true) {
     var oldPause = window.player.pause;
     window.player.pause = function () {
-      if (new Error().stack.includes("loginVersionEveryPlayInternval")) {
-        throw "\n陈睿: 你所热爱的，就是你的生活。\n蒙古上单: 你 ( )什么时候( )啊？";
+      if (
+        new Error().stack.includes("loginVersionEveryPlayInternval")
+        || new Error().stack.includes("loginVersionBackBlock")
+        || new Error().stack.includes("loginVersionMultiEveryPlay")
+        || new Error().stack.includes("loginVersionForce")
+        || new Error().stack.includes("loginVersionWechat")
+      ) {
+        console.log("\n陈睿: 你所热爱的，就是你的生活。\n蒙古上单: 你 ( )什么时候( )啊？");
+        return function(){};
+      }else{
+        return oldPause;
       }
-      return oldPause();
     };
   }
 
@@ -46,3 +62,4 @@
   var targ = document.getElementsByTagName("head")[0] || document.body || document.documentElement;
   targ.appendChild(styleNode);
 })();
+
